c4503c3b5b867c8e6e784257bc825b27c6079dc5
Lab PC <info@freemocap.org>
2025-09-24 11:05:41 -0600

    lab pc updates, add calculate mean

---
+++ skellyclicker/scripts/calculate_mean.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+
+if __name__=='__main__':
+    labels_path = Path("/home/scholl-lab/ferret_recordings/session_2025-07-01_ferret_757_EyeCameras_P33_EO5/clips/1m_20s-2m_20s/mocap_data/dlc_output/skellyclicker_machine_labels_iteration_13.csv")
+    df = pd.read_csv(labels_path)
+    print(df.head(5))
+    print(df.columns)
+
+    # for eye videos:
+    # selected_columns_x = [column for column in df.columns if "p" in column and "_x" in column]
+    # selected_columns_y = [column for column in df.columns if "p" in column and "_y" in column]
+    # print(selected_columns_y)
+
+    # df['eye_mean_x'] = df[selected_columns_x].mean(axis=1)
+    # df['eye_mean_y'] = df[selected_columns_y].mean(axis=1)
+
+    # print(df.head(5))
+
+    # df.to_csv(labels_path.parent.parent / "eye_data_with_mean.csv")
+
+    #  for mocap videos:
+    selected_columns_x = ['nose_x','left_cam_tip_x', 'right_cam_tip_x', 'base_x', 'left_eye_x', 'right_eye_x', 'left_ear_x', 'right_ear_x']
+    selected_columns_y = ['nose_y','left_cam_tip_y', 'right_cam_tip_y', 'base_y', 'left_eye_y', 'right_eye_y', 'left_ear_y', 'right_ear_y']
+
+    print(selected_columns_y)
+
+    df['head_mean_x'] = df[selected_columns_x].mean(axis=1)
+    df['head_mean_y'] = df[selected_columns_y].mean(axis=1)
+
+    print(df.head(5))
+
+    df.to_csv(labels_path.parent.parent / "head_data_with_mean.csv")
+
+
