040481d085bdd34c7ba921b789338cd0167af901
Lab PC <info@freemocap.org>
2025-10-17 15:56:27 -0600

    suppress matplotlib and PIL warnings

--- skellyclicker/__main__.py
+++ skellyclicker/__main__.py
@@ -1,3 +1,14 @@
+import matplotlib.pyplot as plt
+import logging
+import os
+
+plt.set_loglevel('WARNING')
+logging.getLogger('PIL').setLevel(logging.WARNING)
+
+# https://numexpr.readthedocs.io/en/latest/user_guide.html#threadpool-configuration
+# os.environ['NUMEXPR_MAX_THREADS'] = 16
+# os.environ['NUMEXPR_NUM_THREADS'] = 16
+
 from skellyclicker.ui.skellyclicker_ui import SkellyClickerUi
 
 if __name__ == "__main__":
