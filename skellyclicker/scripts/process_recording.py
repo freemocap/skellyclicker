a74ebc3c8fb597a8fba299bfa5c15aa7af83f723
Lab PC <info@freemocap.org>
2026-02-24 10:44:32 -0700

    fix bug looking for video to flip

--- skellyclicker/scripts/process_recording.py
+++ skellyclicker/scripts/process_recording.py
@@ -52,9 +52,9 @@ def run_all_models(recording_path: Path, include_eye: bool, flip_eye_0: bool = F
         print("Processing eye videos...")
         process_recording(video_folder=eye_video_path, deeplabcut_folder=best_eye_model_folder, output_folder=eye_data_path)
         if flip_eye_0:
-            flip_eye0_video(eye_data_folder=eye_data_path)  # use this for october recordings
+            flip_eye0_video(eye_data_folder=eye_data_path)
         if flip_eye_1:
-            flip_eye1_video(eye_data_folder=eye_data_path)  # use this for july recordings
+            flip_eye1_video(eye_data_folder=eye_data_path)
         if flip_eye_0 or flip_eye_1:
             print("standard eye video processed, processing flipped eye videos")
             flipped_eye_videos_path = eye_video_path / "flipped_eye_videos"
@@ -62,7 +62,7 @@ def run_all_models(recording_path: Path, include_eye: bool, flip_eye_0: bool = F
         print("eye videos processed")
     else:
         best_mocap_model_folder = "/home/scholl-lab/deeplabcut_data/head_body_noeyecam_v0"
-    print("Processing mocap videos...")
+    # print("Processing mocap videos...")
     process_recording(video_folder=mocap_video_path, deeplabcut_folder=best_mocap_model_folder)
     print("mocap videos processed")
     print("Processing mocap videos with toy model...")
@@ -71,10 +71,10 @@ def run_all_models(recording_path: Path, include_eye: bool, flip_eye_0: bool = F
 
 if __name__=="__main__":
     # use /full_recording or /clips/{clip_name}
-    recording_path = Path("/home/scholl-lab/ferret_recordings/session_2025-07-09_ferret_753_EyeCameras_P41_E13/full_recording")
+    recording_path = Path("/home/scholl-lab/ferret_recordings/session_2025-07-11_ferret_757_EyeCamera_P43_E15__1/clips/0m_37s-1m_37s")
     include_eye = True
 
-    if "757" in recording_path:
+    if "757" in str(recording_path):
         flip_eye_0 = False
         flip_eye_1 = True
     else:
